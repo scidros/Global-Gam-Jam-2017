f29dd852409d88335a6ba9af400fc6199e18d057
Luca Neri <lucasmodzita@gmail.com>
2017-01-21 13:10:44 +0100

    - New gitignore - Accelerometer

---
+++ GlobalGameJam/Assets/Script/ForceFromAccelerometer.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class ForceFromAccelerometer : MonoBehaviour
+{
+    public float maxVelocity = 5;
+
+    private Rigidbody2D rb;
+
+    private const float GRAVITY = 50;
+    private float gx, gy;
+    
+    private void Awake()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        gx = Input.acceleration.x * GRAVITY;
+        gy = Input.acceleration.y * GRAVITY;
+    }
+
+    private void FixedUpdate()
+    {
+        Physics2D.gravity = new Vector2(gx, gy);
+        rb.velocity = new Vector2(Mathf.Clamp(rb.velocity.x, -maxVelocity, maxVelocity), Mathf.Clamp(rb.velocity.y, -maxVelocity, maxVelocity));
+    }
+}
